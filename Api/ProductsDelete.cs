f354fbb285913e6ff221569979e1da9d07508e65
Anthony Chu <anthony@anthonychu.ca>
2021-12-01 00:40:37 +0000

    Sort usings

--- Api/ProductsDelete.cs
+++ Api/ProductsDelete.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
