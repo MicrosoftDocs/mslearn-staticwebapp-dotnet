f58861c68ee8d7f17b663aca04146b1c6cfffd2e
Aaron Powell <me@aaron-powell.com>
2023-06-23 15:14:16 +1000

    Using the aspnet integration for better types and updating dependencies

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,24 +1,18 @@
-﻿using Microsoft.Azure.Functions.Worker;
+﻿using Api;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Api;
-
-public class Program
-{
-    public static void Main()
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults(workerApplication =>
     {
-        var host = new HostBuilder()
-            .ConfigureFunctionsWorkerDefaults(workerApplication =>
-            {
-                workerApplication.UseAspNetCoreIntegration();
-            })
-            .ConfigureServices(services =>
-            {
-                services.AddSingleton<IProductData, ProductData>();
-            })
-            .Build();
+        workerApplication.UseAspNetCoreIntegration();
+    })
+    .ConfigureAspNetCoreIntegration()
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<IProductData, ProductData>();
+    })
+    .Build();
 
-        host.Run();
-    }
-}
+host.Run();
